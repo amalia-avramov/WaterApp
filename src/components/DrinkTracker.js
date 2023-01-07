ab0d1d96897c4e0d98c223e10a3577ef0ef2a027
Dan <gramescudan444@gmail.com>
2023-01-07 19:08:32 +0200

    Updated Droplet

--- src/components/DrinkTracker.js
+++ src/components/DrinkTracker.js
@@ -1,22 +1,29 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 
 const DrinkTracker = () => {
     const [mlConsumed, setMlConsumed] = useState(0);
+    const [inputValue, setInputValue] = useState('');
 
-    const updateMlConsumed = (ml) => {
-        setMlConsumed(ml);
+    const updateMlConsumed = () => {
+        setMlConsumed(mlConsumed + Number(inputValue));
+        setInputValue('');
     }
 
     return (
         <View style={styles.container}>
             <Text style={styles.text}>Milliliters consumed: {mlConsumed}</Text>
-            <View style={styles.progressBar}>
-                <View style={[styles.fill, { width: `${mlConsumed / 2000 * 100}%` }]} />
+            <View style={styles.progressBarContainer}>
+                <View style={[styles.fill, { height: `${mlConsumed / 2000 * 100}%` }]}>
+                    <Text style={styles.progressText}>{mlConsumed}</Text>
+                </View>
+                <View style={styles.empty} />
             </View>
             <TextInput
                 style={styles.input}
                 placeholder="Enter milliliters consumed"
+                value={inputValue}
+                onChangeText={setInputValue}
                 keyboardType="numeric"
             />
             <Button
@@ -37,26 +44,48 @@ const styles = StyleSheet.create({
         fontSize: 20,
         marginBottom: 20,
     },
-    progressBar: {
-        width: '80%',
-        height: 40,
-        borderRadius: 20,
+    progressBarContainer: {
+        width: 100,
+        height: 300,
+        borderRadius: 50,
         borderWidth: 2,
         borderColor: '#ddd',
         overflow: 'hidden',
+        position: 'relative',
     },
     fill: {
         height: '100%',
         backgroundColor: 'blue',
+        borderTopLeftRadius: 50,
+        borderTopRightRadius: 50,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    empty: {
+        height: '100%',
+        backgroundColor: '#ddd',
+        borderBottomLeftRadius: 50,
+        borderBottomRightRadius: 50,
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+    },
+    progressText: {
+        color: 'white',
     },
     input: {
-            width: '80%',
-            height: 40,
-            borderColor: 'gray',
-            borderWidth: 1,
-            marginTop: 20,
-            padding: 10,
-        },
-    });
+        width: '80%',
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginTop: 20,
+        padding: 10,
+    },
+});
 
 export default DrinkTracker;
