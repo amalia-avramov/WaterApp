98e998eb108cdc2b48739f242a5c6a4c81215215
Amalia Avramov <amalia.avramov@yahoo.com>
2023-01-11 10:56:01 +0200

    Add possibility to edit your profile info

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,17 +1,20 @@
 import {getAuth, signOut} from "firebase/auth";
 import app from "../config/firebase";
 import {AntDesign} from "@expo/vector-icons";
-import {StyleSheet, Text, TouchableWithoutFeedback, View} from "react-native";
+import {StyleSheet, Text, View} from "react-native";
 import {useAuth} from "../config/useAuth";
 import {useEffect, useState} from "react";
-import {collection, getDocs, getFirestore} from "firebase/firestore";
+import {collection, doc, getDocs, getFirestore, updateDoc} from "firebase/firestore";
 import {theme} from "../components/theme";
+import TextInput from "../components/TextInput";
+import Button from "../components/Button";
 
 const auth = getAuth(app);
 const db = getFirestore(app);
 
 export function Profile({navigation}) {
     const {user} = useAuth();
+    const [userId, setUserId] = useState('')
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('')
@@ -19,6 +22,7 @@ export function Profile({navigation}) {
     const [drinkingWater, setDrinkingWater] = useState(0);
     const [dateOfBirth, setDateOfBirth] = useState('');
     const [gender, setGender] = useState('');
+    const [isEditing, setIsEditing] = useState(false);
 
     useEffect(() => {
         const fetchPost = async () => {
@@ -34,14 +38,22 @@ export function Profile({navigation}) {
                     setDrinkingWater(newCurrentUser.drinkingWater);
                     setWeight(newCurrentUser.weight.value);
                     setGender(newCurrentUser.gender)
+                    setUserId(newCurrentUser.id)
                 })
         }
         fetchPost().catch((error) => console.log(error));
     }, [user])
 
+    const updateData = async () => {
+        const docRef = doc(db, "users", userId);
+        await updateDoc(docRef, {
+            weight: {value: weight, error: ''},
+            drinkingWater: drinkingWater,
+        })
+        setIsEditing(false);
+    }
     return (
         <View>
-
             <View style={{backgroundColor: '#6baafd', height: 100}}>
                 <View style={{margin: 10, alignItems: 'flex-end'}}>
                     <AntDesign name="logout" size={32} color="black"
@@ -70,35 +82,68 @@ export function Profile({navigation}) {
                 <Text style={styles.name}>{firstName}</Text>
                 <Text style={styles.name}>{lastName}</Text>
             </View>
-            <TouchableWithoutFeedback>
-                <View style={styles.mainCardView}>
-                    <View style={{flexDirection: 'column'}}>
-                        <Text
-                            style={styles.text}>
-                            Email: {email}
+
+            <View style={styles.mainCardView}>
+                <View style={{flexDirection: 'column'}}>
+                    <Text
+                        style={styles.text}>
+                        Email: {email}
+                    </Text>
+                    <Text
+                        style={styles.text}>
+                        Date of birth: {dateOfBirth}
+                    </Text>
+                    <Text
+                        style={styles.text}>
+                        Gender: {gender}
+                    </Text>
+                    <View style={{flexDirection:'row'}}>
+                        <Text style={styles.text}>
+                            Weight:
                         </Text>
-                        <Text
-                            style={styles.text}>
-                            Weight: {weight}
-                        </Text>
-                        <Text
-                            style={styles.text}>
-                            Date of birth: {dateOfBirth}
-                        </Text>
-                        <Text
-                            style={styles.text}>
-                            Goal: {drinkingWater}L
-                        </Text>
-                        <Text
-                            style={styles.text}>
-                            Gender: {gender}
-                        </Text>
+                        {!isEditing ?
+                            <Text style={styles.text}>{weight}</Text> :
+                            <TextInput
+                                returnKeyType="done"
+                                value={weight.toString()}
+                                onChangeText={(text) => setWeight(Number(text))}
+                                keyboardType="number-pad"
+                                style={{width: 100}}
+                            />}
                     </View>
+                    <View style={{flexDirection:'row'}}>
+                        <Text
+                            style={styles.text}>
+                            Goal:
+                        </Text>
+                        {!isEditing ?
+                            <Text style={styles.text}>{drinkingWater}L</Text> :
+                            <TextInput
+                                returnKeyType="done"
+                                value={drinkingWater.toString()}
+                                onChangeText={(text) => setDrinkingWater(Number(text))}
+                                keyboardType="number-pad"
+                                style={{width: 100}}
+                            />}
+                    </View>
+                    {isEditing && <Button mode="contained" onPress={updateData} style={{width: 150}}>
+                        Save
+                    </Button>}
                 </View>
-            </TouchableWithoutFeedback>
-            <View style={{backgroundColor: '#6baafd', borderRadius:25, width:50, height:50, alignItems: 'center', marginLeft:'auto'}}>
-                <AntDesign name='edit' size={24} style={{margin:10}}/>
             </View>
+            {
+                !isEditing && <View style={{
+                    backgroundColor: '#6baafd',
+                    borderRadius: 25,
+                    width: 50,
+                    height: 50,
+                    alignItems: 'center',
+                    marginLeft: 'auto',
+                    marginRight: 5,
+                }}>
+                    <AntDesign name='edit' size={24} style={{margin: 10}} onPress={() => setIsEditing(true)}/>
+                </View>
+            }
         </View>
     )
 }
@@ -114,11 +159,10 @@ const styles = StyleSheet.create({
         color: theme.colors.primary
     },
     container: {
-        flex: 1,
         backgroundColor: '#fff',
     },
     mainCardView: {
-        height: '80%',
+        height: 450,
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#fff',
