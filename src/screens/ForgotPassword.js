dacda40b279ec72459f6cd403ad329b0224cb2d3
Amalia Avramov <amalia.avramov@yahoo.com>
2023-01-11 21:06:50 +0200

    Reset password implementation

--- src/screens/ForgotPassword.js
+++ src/screens/ForgotPassword.js
@@ -1,14 +1,15 @@
-import React, { useState } from 'react';
-import { Button } from 'react-native';
+import React, {useState} from 'react';
+import {Button, StyleSheet, Text} from 'react-native';
 import app from "../config/firebase";
-import {getAuth} from "firebase/auth";
+import {getAuth, sendPasswordResetEmail} from "firebase/auth";
 import Background from "../components/Background";
-import Paragraph from "../components/Paragraph";
 import TextInput from "../components/TextInput";
 import BackButton from "../components/BackButton";
+import alert from "react-native-web/dist/exports/Alert";
 
-const auth= getAuth(app);
-function ForgotPasswordScreen({ navigation }) {
+const auth = getAuth(app);
+
+function ForgotPasswordScreen({navigation}) {
     const [email, setEmail] = useState('');
     const [newPassword, setNewPassword] = useState('');
     const [confirmNewPassword, setConfirmNewPassword] = useState('');
@@ -19,22 +20,17 @@ function ForgotPasswordScreen({ navigation }) {
             alert('Please enter your email.');
             return;
         }
-
-        // Validate new password and confirm new password
-        if (!newPassword || !confirmNewPassword) {
-            alert('Please enter and confirm your new password.');
-            return;
-        }
-
-        if (newPassword !== confirmNewPassword) {
-            alert('The new password and confirm new password fields do not match.');
-            return;
-        }
-
         try {
             // Send request to Firebase to reset password with the email provided
-            await auth.sendPasswordResetEmail(email);
+            sendPasswordResetEmail(auth, email)
+                .then(() => {
+                    alert('Reset email was sent successfully.');
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
 
+                });
             // If successful, navigate to Home screen
             navigation.navigate('Login');
         } catch (error) {
@@ -46,27 +42,25 @@ function ForgotPasswordScreen({ navigation }) {
     return (
         <Background>
             <BackButton goBack={navigation.goBack}/>
-            <Paragraph>Forgot Password</Paragraph>
+            <Text style={styles.text}>Forgot Password</Text>
             <TextInput
                 placeholder="Email"
                 value={email}
                 onChangeText={setEmail}
             />
-            <TextInput
-                placeholder="New Password"
-                value={newPassword}
-                onChangeText={setNewPassword}
-                secureTextEntry={true}
-            />
-            <TextInput
-                placeholder="Confirm New Password"
-                value={confirmNewPassword}
-                onChangeText={setConfirmNewPassword}
-                secureTextEntry={true}
-            />
-            <Button title="Reset Password" onPress={handleResetPassword} />
+            <Button title="Reset Password" onPress={handleResetPassword}/>
         </Background>
     );
 }
 
 export default ForgotPasswordScreen;
+
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 15,
+        fontWeight: 'bold',
+        lineHeight: 21,
+        textAlign: 'center',
+        marginBottom: 12,
+    },
+})
