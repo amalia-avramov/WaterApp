e1ae005e6047621e8d8f773e9abbaf475fe09c7a
Amalia Avramov <amalia.avramov@yahoo.com>
2023-01-12 09:19:50 +0200

    Start page fix

--- src/screens/Start.js
+++ src/screens/Start.js
@@ -37,7 +37,7 @@ const styles = StyleSheet.create({
         flex: 1,
         width: '100%',
         height: '50%',
-        backgroundColor: theme.colors.surface,
+        backgroundColor: '#fff',
     },
     container: {
         flex: 1,
